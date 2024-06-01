846128f4eac2508759b89ca2327cff832cbbdbc6
MapleMacchiato <gabriel.b.stein@gmail.com>
2024-06-01 17:57:47 -0400

    created func to copy static dir

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import os
+import shutil
 from textnode import TextNode
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    copy_dir("test_source", "test_target")
-    print(node)
+    target_dir, source_dir = "public", "static"
+    if os.path.exists(target_dir):
+        shutil.rmtree(target_dir)
+    os.mkdir(target_dir)
+    copy_dir(source_dir, target_dir)
 
 def copy_dir(source, target):
-    print(os.listdir("../"))
-    
-
-
-
+    source_items = os.listdir(source)
+    target_items = os.listdir(target)
+    print(f"SOURCE{source_items} TARGET{target_items}")
+    for item in source_items:
+        source_path = os.path.join(source, item)
+        target_path = os.path.join(target, item)
+        if os.path.isfile(source_path):
+            print(f"Copying {source_path} to {target_path}")
+            shutil.copy(source_path, target_path)
+            continue
+        os.mkdir(target_path)
+        copy_dir(source_path, target_path)
 main()
 
 
