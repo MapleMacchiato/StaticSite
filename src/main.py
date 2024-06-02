2d1570c7d50a7d0ed44294bf1298bd6ad39ad78a
MapleMacchiato <gabriel.b.stein@gmail.com>
2024-06-01 20:36:57 -0400

    added func to recursively add pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 from textnode import TextNode
 from markdown_blocks import markdown_to_html_node
 def main():
@@ -9,10 +10,14 @@ def main():
     os.mkdir(target_dir)
     copy_dir(source_dir, target_dir)
     
-    from_path = "content/index.md"
+    #from_path = "content/index.md"
     template_path = "template.html"
-    dest_path = "public/index.html"
-    generate_page(from_path, template_path, dest_path)
+    #dest_path = "public/index.html"
+    #generate_page(from_path, template_path, dest_path)
+    from_dir = "content"
+    dest_dir = "public"
+    generate_pages_recursive(from_dir, template_path, dest_dir)
+
 
 def copy_dir(source, target):
     source_items = os.listdir(source)
@@ -27,36 +32,45 @@ def copy_dir(source, target):
         os.mkdir(target_path)
         copy_dir(source_path, target_path)
 
-def extract_title(markdown):
-    lines = markdown.split("\n")
-    if lines[0].startswith("# "):
-        return lines[0].strip("# ")
-    else:
-        raise Exception("All pages need a single h1 header")
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    
-    md_text = ""
-    with open(from_path, "r") as f:
-        md_text = f.read()
-    f.close()
-    
-    title = extract_title(md_text)
-    template = ""
-    with open(template_path, "r") as f:
-        template = f.read()
-    f.close()
-    
-    html_nodes = markdown_to_html_node(md_text)
-    html = html_nodes.to_html()
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
-    
-    f = open(dest_path, "w")
-    f.write(template)
-    f.close()
 
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found")
 
 main()
 
