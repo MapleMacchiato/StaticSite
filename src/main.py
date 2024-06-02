515b5a1e5b1fb882641ffe883c2cab1b0e084f29
MapleMacchiato <gabriel.b.stein@gmail.com>
2024-06-01 20:11:16 -0400

    created function to generate a static page

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 import os
 import shutil
 from textnode import TextNode
-
+from markdown_blocks import markdown_to_html_node
 def main():
     target_dir, source_dir = "public", "static"
     if os.path.exists(target_dir):
         shutil.rmtree(target_dir)
     os.mkdir(target_dir)
     copy_dir(source_dir, target_dir)
+    
+    from_path = "content/index.md"
+    template_path = "template.html"
+    dest_path = "public/index.html"
+    generate_page(from_path, template_path, dest_path)
 
 def copy_dir(source, target):
     source_items = os.listdir(source)
@@ -21,6 +26,38 @@ def copy_dir(source, target):
             continue
         os.mkdir(target_path)
         copy_dir(source_path, target_path)
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    if lines[0].startswith("# "):
+        return lines[0].strip("# ")
+    else:
+        raise Exception("All pages need a single h1 header")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
+    md_text = ""
+    with open(from_path, "r") as f:
+        md_text = f.read()
+    f.close()
+    
+    title = extract_title(md_text)
+    template = ""
+    with open(template_path, "r") as f:
+        template = f.read()
+    f.close()
+    
+    html_nodes = markdown_to_html_node(md_text)
+    html = html_nodes.to_html()
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    
+    f = open(dest_path, "w")
+    f.write(template)
+    f.close()
+
+
 main()
 
 
